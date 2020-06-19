815c72f4af31672afec83dbf9f907654871a6d8e
kwamkert <tomriddle477@gmail.com>
2020-06-19 23:57:42 +0000

    urlify problem

---
+++ urlify.py
@@ -0,0 +1,15 @@
+#urlify string
+
+#count number of spaces
+def count_spaces(string, length):
+    spaces = 0
+    for i in range(0. length):
+        if string[i] == ' ':
+            spaces += 1
+    return spaces
+
+
+
+def urlify(string, truelength):
+
+
