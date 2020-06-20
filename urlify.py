cc68bf8c2923eb4d46d26f04575968270731694f
kwamkert <tomriddle477@gmail.com>
2020-06-20 01:25:18 +0000

    permutation palindrome added

--- urlify.py
+++ urlify.py
@@ -3,7 +3,7 @@
 #count number of spaces
 def count_spaces(string, length):
     spaces = 0
-    for i in range(0. length):
+    for i in range(0, length):
         if string[i] == ' ':
             spaces += 1
     return spaces
@@ -11,5 +11,23 @@ def count_spaces(string, length):
 
 
 def urlify(string, truelength):
+    spaces = count_spaces(string, truelength)
+    indexCount = truelength + (spaces*2)
 
+    count = truelength -1
+    string = list(string)
+    while count >= 0:
+        if string[indexCount -1] == ' ':
+            string[indexCount -1] = '0'
+            string[indexCount -2] = '2'
+            string[indexCount -3] = '%'
+            count -= 1
+            indexCount -=1
+            print(string)
+        else:
+            string[indexCount -1] =  string[count]
+            indexCount -= 1
+            count -=1
+    return "".join(string)
 
+print(urlify('Mr John Smith    ',13))
